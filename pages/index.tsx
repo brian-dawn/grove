e5911b8d38759bda7376fbd6a61c51d114324f1b
Brian Dawn <brian.t.dawn@gmail.com>
2020-05-23 12:32:13 -0500

    Pretty print the date.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,29 @@
-
-import { GetStaticProps, GetStaticPaths, GetServerSideProps } from 'next'
-import Head from 'next/head'
-import fetch from '../libs/fetch'
-import useSWR from 'swr'
-import { Data } from './api/notes'
-import { Note } from '../libs/model'
-
+import { GetStaticProps, GetStaticPaths, GetServerSideProps } from "next";
+import Head from "next/head";
+import fetch from "../libs/fetch";
+import useSWR from "swr";
+import { Data } from "./api/notes";
+import { Note } from "../libs/model";
 
 export default function Home() {
-  const { data, error } = useSWR<Note[]>('/api/notes', fetch)
+  const { data, error } = useSWR<Note[]>("/api/notes", fetch);
 
-  if (error) return <div>failed to load</div>
-  if (!data) return <div>loading...</div>
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
 
-  return(<div>
-    hello {data.map(note => {
-    return (
+  return (
     <div>
-      <div>{note.timestamp}</div>
-      <div>{note.content}</div>
+      {data.map((note) => {
+        const date = new Date(note.timestamp);
+        return (
+          <div>
+            ####################
+            <div>{date.toLocaleString()}</div>
+            <div>{note.content}</div>
+            ####################
+          </div>
+        );
+      })}
     </div>
-
-    )
-  })}!
-  </div>)
+  );
 }
