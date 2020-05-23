d0cfa4904add3b3a080a0da5a38af8a13b8f20b2
Brian Dawn <brian.t.dawn@gmail.com>
2020-05-23 09:49:47 -0500

    Wire up TS and SWR.

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,11 @@
 
 import { NextApiRequest, NextApiResponse } from 'next'
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ text: 'Hello' })
+
+export type Data = {
+  name: string
+}
+
+export default (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  res.status(200).json({ name: `Hello${req.url}` })
 }
