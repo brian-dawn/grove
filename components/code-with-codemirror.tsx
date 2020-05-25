aecccf2dc3db69638e45f1f2371de8a78b58a1b6
Brian Dawn <brian.t.dawn@gmail.com>
2020-05-25 11:44:32 -0500

    Actually enable markdown mode.

--- components/code-with-codemirror.tsx
+++ components/code-with-codemirror.tsx
@@ -1,24 +1,35 @@
 import React, { Component } from "react";
+import { useState } from "react";
 import { Controlled as CodeMirror } from "react-codemirror2";
-import "codemirror/mode/javascript/javascript";
+import "codemirror/mode/markdown/markdown";
 import "codemirror/lib/codemirror.css";
+import "codemirror/addon/hint/show-hint";
+import "codemirror/addon/hint/show-hint.css";
 import "codemirror/theme/base16-light.css";
 import "codemirror/keymap/vim";
 import "codemirror/keymap/emacs";
 
-export default (props: any) => (
-  <div>
-    <CodeMirror
-      {...props}
-      value={props.value}
-      editorDidMount={(editor) => {
-        editor.setSize("100%", "6em");
-      }}
-      options={{
-        theme: "base16-light",
-        mode: "markdown",
-        keyMap: "vim",
-      }}
-    />
-  </div>
-);
+export default (props: any) => {
+  const [editor, setEditor] = useState(undefined);
+  return (
+    <div>
+      <CodeMirror
+        {...props}
+        value={props.value}
+        editorDidMount={(ed) => {
+          setEditor(ed);
+          ed.setSize("100%", "6em");
+        }}
+        options={{
+          theme: "base16-light",
+          mode: "markdown",
+          keyMap: "vim",
+
+          extraKeys: {
+            "Ctrl-Space": "autocomplete",
+          },
+        }}
+      />
+    </div>
+  );
+};
